b96aa23dac49e44b6a12a94e6f01bef3f3f20da8
Gabrielle Castro <gabriellebcastro@gmail.com>
2023-01-26 19:06:32 -0300

    adicionando router & finalizando

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { routes } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 
@@ -7,6 +9,7 @@ import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { HttpClientModule } from '@angular/common/http';
 
 registerLocaleData(localePt, 'pt')
 
@@ -18,7 +21,9 @@ registerLocaleData(localePt, 'pt')
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'pt'},
